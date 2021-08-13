74e02c8d4dda21b002a83a91dbd6a5cc597b66ba
conconrob <connortprobertson@gmail.com>
2021-08-13 08:41:20 -0700

    feat: setup cra

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}></div>
+  );
+}
+
+export default App;
